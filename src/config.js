aa08c5b319f120e40c373461f5b0261e6ae554ec
troyeguo <guo362429@163.com>
2021-02-02 17:11:30 +0800

    first commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const currentUrl = document.URL.split("#")[0];
 module.exports = {
   devHost: "http://localhost:3001",
-  prodHost: currentUrl.substring(0, currentUrl.length - 1),
+  prodHost: "https://coodopay.herokuapp.com/",
 };
