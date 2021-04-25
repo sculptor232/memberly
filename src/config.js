8a74104c0d9450de460e701aa64ecbb1f01a0a82
unknown <guo362429@gmail.com>
2021-04-25 13:16:11 +0800

    fix bug

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const currentUrl = document.URL.split("#")[0];
 module.exports = {
   devHost: "http://localhost:3001",
   prodHost:
-    currentUrl.indexOf("vip") > -1
+    currentUrl.indexOf("memberly") > -1
       ? "https://pay.960960.xyz"
       : "https://coodopay.herokuapp.com",
 };
